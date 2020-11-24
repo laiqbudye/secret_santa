4a42f4da6a4a09aee0e44956afc25f8aa84758d0
laiqbudye <laiqbudye@gmail.com>
2020-11-24 20:49:17 +0530

    Backend || DB || added logic for database connection

---
+++ config/db.js
@@ -0,0 +1,24 @@
+/*
+this file contains db connecion logic 
+ */
+
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+           useNewUrlParser: true,
+           useCreateIndex: true,
+           useUnifiedTopology: true
+        });
+        console.log("Database Connected...")
+    } catch (error) {
+       console.error(error.message);
+       //exit process due to fail
+       process.exit(1);        
+    }
+}
+
+module.exports = connectDB;
