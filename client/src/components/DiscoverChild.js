c2650767c61df88d351e77a2f35172904b3d4e4b
laiqbudye <laiqbudye@gmail.com>
2020-11-30 19:05:37 +0530

    FrontEnd || added DiscoverChild Component

---
+++ client/src/components/DiscoverChild.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DiscoverChild = (props) => {
+    console.log(props)
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default DiscoverChild;
